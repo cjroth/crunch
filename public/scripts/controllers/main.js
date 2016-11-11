a8c2266d7f842dcbe32512f712b5228e2315f40d
Chris Roth <chris@cjroth.com>
2016-11-10 21:57:51 -0500

    remove console.log

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -5,7 +5,6 @@ angular
             .getVariableListWithOrder('349d49', 'hier')
             .then((variableList) => {
                 $scope.graph = populateVariableGraph(variableList.index, variableList.order.graph)
-                console.log($scope.graph)
                 $scope.$apply()
             })
     })
