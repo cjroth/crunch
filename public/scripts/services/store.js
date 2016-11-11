01757194c80bc3bb2c8bcf882c8f5498c2606115
Chris Roth <chris@cjroth.com>
2016-11-10 21:54:46 -0500

    some more docs

--- public/scripts/services/store.js
+++ public/scripts/services/store.js
@@ -1,9 +1,17 @@
 class Store {
+
     constructor(Restangular) {
         this.Restangular = Restangular
         this.Restangular.setBaseUrl(window.location.origin)
         this.datasets = {}
     }
+
+    // Get a variable list (an object in the format specified in
+    // `/fixtures/variables.json`) and optionally an order name. If you specify
+    // an order name, it will get the specified order from the API set it the
+    // `order` property of the variable list object.
+    //
+    // Returns a promise.
     getVariableListWithOrder(datasetId, orderName) {
         return new Promise((resolve, reject) => {
             this.Restangular
@@ -24,6 +32,7 @@ class Store {
                 })
         })
     }
+
 }
 
 angular
